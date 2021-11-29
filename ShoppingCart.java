97bd9aa323bf235a78a8eacdb37e5952a2ad524f
Joe <24jdburks@macon.k12.mo.us>
2021-11-29 10:42:01 -0600

    sixth push

---
+++ ShoppingCart.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+/**
+ *
+ * @author anshenoy
+ */
+public class ShoppingCart {
+    public static void main (String[] args){
+        String custName = "Steve Smith";
+        String firstName = custName.substring(0,5);
+        int spaceIdx = custName.indexOf(' ');
+        System.out.println(spaceIdx);
+        System.out.println(firstName);
+        // Get the index of the space character (" ") in custName. 
+        
+
+        // Use the substring method parse out the first name and print it.
+
+
+    }
+
+    
+}
